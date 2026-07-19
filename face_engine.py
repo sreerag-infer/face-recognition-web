28f9df189df356a55be135e60e32f758e003b753
sreerag-infer <sb30000002@gmail.com>
2026-07-19 11:13:41 +0530

    Bundle InsightFace model

--- face_engine.py
+++ face_engine.py
@@ -58,6 +58,7 @@ class FaceEngine:
 
         self.model = insightface.app.FaceAnalysis(
             name="buffalo_s",
+            root="./models",
             providers=providers
         )
         logger.info("Preparing InsightFace model...")
